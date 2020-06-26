eaf31e7d04aeb1ab9831e703e60ae17679e1169c
pgqcoding <ethan-pang@qq.com>
2020-06-26 16:45:15 +0800

    init

---
+++ design-pattern-strategy/src/main/java/com/pgqcoding/demo/strategy/DesignPatternStrategyApplication.java
@@ -0,0 +1,17 @@
+package com.pgqcoding.demo.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 设计模式策略应用
+ *
+ * @author Guoqing.Pang
+ * @date 2020/06/13
+ */
+@SpringBootApplication
+public class DesignPatternStrategyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternStrategyApplication.class, args);
+    }
+}
